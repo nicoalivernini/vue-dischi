299c6d7962a259e186e925b83cc838e08f0503cb
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-04-09 15:13:47 +0200

    Creazione file di base

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// Attravero una chiamata AJAX avere a disposizione una decina di dischi musicali.
+// https://flynn.boolean.careers/exercises/api/array/music
+// BONUS
+// Creare una select con tutti i generi dei dischi. In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
+// BONUS 2
+// Ordinare i dischi per anno di uscita.
