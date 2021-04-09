cafd0191af5c63f28627764344254beef652ee4b
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-04-09 16:28:36 +0200

    Aggiornamento

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,13 +12,25 @@ var app = new Vue ({
   el: '#root',
   data: {
     albums: [],
+    generi: [],
   },//Chiusura Data
 
   mounted() {
+    //Chiamata per data albums
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
     .then((response) => {
       this.albums = (response.data.response)
     });
+
+    //Chiamata per data generi
+    //for (var i = 0; i < 10; i++) {
+      // axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      // .then((response) => {
+      //   //this.generi = (response.data.response[i].genre)
+      //   console.log(response.data.response[3].genre);
+      // });
+    //}
+
   }, //Chiurusa Mounted
 
 })//Chiusura Vue
